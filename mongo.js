97225ceb0c1e03f42b51288d54b50bf320a210e9
Nick Jones <nico.rjones@outlook.com>
2024-04-06 03:34:49 -0700

    exercise 3.12

---
+++ mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+const connection = mongoose.connection
+const model = mongoose.model
+
+
+if (process.argv.length < 5) {
+    console.log('Usage: node mongo.js <password> <name> <number>');
+    process.exit(1);
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://sinisterthought:${password}@cluster0.wd7qv5o.mongodb.net/contactsApp?retryWrites=true&w=majority`
+
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true } )
+    .then(() => {
+        console.log('Connected to MongoDB')
+
+
+        const contactSchema = new Schema({
+            name: String,
+            number: String,
+        })
+
+
+        const Contact = model('Contact', contactSchema)
+
+        const contact = new Contact({
+            name: name,
+            number: number
+        })
+
+        return contact.save()
+    })
+    .then(() => {
+        console.log('Contact saved successfully')
+        return connection.close()
+    })
+    .catch(error => console.error('Error', error))
+
+//pEaxZhE2vjKjEEo8
