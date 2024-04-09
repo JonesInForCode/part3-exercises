d7769733909a6ebd0a26e7c8c7feec6f9277bc7a
Nick Jones <nico.rjones@outlook.com>
2024-04-09 04:02:55 -0700

    exercise 3.12

--- mongo.js
+++ mongo.js
@@ -5,7 +5,7 @@ const connection = mongoose.connection
 const model = mongoose.model
 
 
-if (process.argv.length < 5) {
+if (process.argv.length < 3) {
     console.log('Usage: node mongo.js <password> <name> <number>');
     process.exit(1);
 }
@@ -16,31 +16,50 @@ const number = process.argv[4]
 
 const url = `mongodb+srv://sinisterthought:${password}@cluster0.wd7qv5o.mongodb.net/contactsApp?retryWrites=true&w=majority`
 
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true } )
+if (process.argv.length === 3) {
+    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected to MongoDB')
-
 
         const contactSchema = new Schema({
             name: String,
             number: String,
         })
 
-
         const Contact = model('Contact', contactSchema)
 
-        const contact = new Contact({
-            name: name,
-            number: number
+        Contact.find({})
+        .then(contacts => {
+            console.log(contacts)
+            return connection.close()
         })
-
-        return contact.save()
-    })
-    .then(() => {
-        console.log('Contact saved successfully')
-        return connection.close()
+        .catch(error => console.error('Error', error))
     })
     .catch(error => console.error('Error', error))
+} else {mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true } )
+.then(() => {
+    console.log('Connected to MongoDB')
+
+
+    const contactSchema = new Schema({
+        name: String,
+        number: String,
+    })
+
+
+    const Contact = model('Contact', contactSchema)
+
+    const contact = new Contact({
+        name: name,
+        number: number
+    })
+
+    return contact.save()
+})
+.then(() => {
+    console.log('Contact saved successfully')
+    return connection.close()
+})
+.catch(error => console.error('Error', error))}
 
 //pEaxZhE2vjKjEEo8
